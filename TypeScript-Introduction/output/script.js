d77d2e7981df8319d10796a96322d3f5b0636db7
Shajib-Dv <mdsojeeb242@gmail.com>
2023-07-18 11:04:44 +0600

    setup done

---
+++ TypeScript-Introduction/output/script.js
@@ -0,0 +1,8 @@
+"use strict";
+/** @format */
+console.log("Hello World !");
+const getName = (firstName, lastName) => {
+    return firstName + " " + lastName;
+};
+const fullName = getName("Shajib", "Hossain");
+console.log(fullName);
